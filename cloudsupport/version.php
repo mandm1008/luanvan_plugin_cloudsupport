983d58094570f5dd6dcc31c4ddcf9c0b7225c2bd
mandm1008 <dominhman311@gmail.com>
2025-07-21 07:33:44 +0000

    update dependencies

--- cloudsupport/version.php
+++ cloudsupport/version.php
@@ -6,3 +6,8 @@ $plugin->version   = 2025071917;            // Định dạng YYYYMMDDXX
 $plugin->requires  = 2023111300;            // Moodle 5.0 (Build: 20231113)
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->release   = '1.0 for Moodle 5.0';
+
+$plugin->dependencies = [
+    'local_webhook' => ANY_VERSION,
+    'tool_objectfs' => ANY_VERSION,
+];
